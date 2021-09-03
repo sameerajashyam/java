8fb667eca3de34aef4d70f1ca560e5cdd825e9df
sameerajashyam <79054143+sameerajashyam@users.noreply.github.com>
2021-09-03 09:42:43 +0530

    Create  taking character  as input.java

---
+++ taking character  as input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class charinput{
+public static void main(String[] arggs)
+{
+Scanner sc=new Scanner(System.in);
+char ch;
+ch=sc.next().charAt(0);
+System.out.println(ch);
+
+}
+}
