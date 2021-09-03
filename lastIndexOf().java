7aaa1a8f77a59e072153628129f17a0a1edced83
sameerajashyam <79054143+sameerajashyam@users.noreply.github.com>
2021-09-03 10:14:59 +0530

    Create lastIndexOf().java

---
+++ lastIndexOf().java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class main
+{
+public static void main(String[] args)
+{
+Scanner input=new Scanner(System.in);
+System.out.println("Enter the string:");
+String s=input.nextLine();
+
+
+System.out.println(s.indexOf("a"));
+System.out.println(s.lastIndexOf("a"));
+
+}
+}
