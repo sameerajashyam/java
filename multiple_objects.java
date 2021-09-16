ae27a528e2c60fb5a7e3227c2903d22bfb6aee82
sameerajashyam <79054143+sameerajashyam@users.noreply.github.com>
2021-09-16 22:48:12 +0530

    Create multiple_objects.java

---
+++ multiple_objects.java
@@ -0,0 +1,12 @@
+class Main
+{
+int x=12;
+public static void main(String[] args)
+{
+Main myobj1=new Main();
+Main myobj2=new Main();
+System.out.println(myobj1.x);
+System.out.println(myobj2.x);
+
+}
+}
