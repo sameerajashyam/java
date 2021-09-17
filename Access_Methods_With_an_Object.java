e2bfca21c1432f80fd1b6688863cddeef4bc78ea
sameerajashyam <79054143+sameerajashyam@users.noreply.github.com>
2021-09-17 09:47:54 +0530

    Create Access_Methods_With_an_Object.java

---
+++ Access_Methods_With_an_Object.java
@@ -0,0 +1,18 @@
+class Main
+{
+
+void full()
+{
+System.out.println("The car is red");
+}
+void speed(int maxSpeed)
+{
+System.out.println("max speed is"+maxSpeed);
+}
+public static void main(String[] args)
+{
+Main myCar=new Main();
+myCar.full();
+myCar.speed(200);
+}
+}
