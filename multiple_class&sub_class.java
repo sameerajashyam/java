4565ba0e43ce206165c4b73e6234bb37040bbe94
sameerajashyam <79054143+sameerajashyam@users.noreply.github.com>
2021-09-17 09:20:39 +0530

    Create multiple_class&sub_class.java

---
+++ multiple_class&sub_class.java
@@ -0,0 +1,16 @@
+class MainClass
+{
+public static void main(String[] args)
+{
+subClass sc=new subClass();
+sc.disp();
+}
+}
+class subClass
+{
+String msg="Hello welcome to classes";
+void disp()
+{
+System.out.println(msg);
+}
+}sub
