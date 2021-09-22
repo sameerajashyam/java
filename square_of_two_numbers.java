1b18e3e2bcf126f0f5d2d75cea2d661c25817ea1
sameerajashyam <79054143+sameerajashyam@users.noreply.github.com>
2021-09-22 07:02:57 +0530

    Create square_of_two_numbers.java

---
+++ square_of_two_numbers.java
@@ -0,0 +1,13 @@
+class main
+{
+ static int square(int num)
+{
+return num*num;
+}
+public static void main(String[] args)
+{
+int result;
+result=square(10);
+System.out.println("Square value is"+result);
+}
+}
